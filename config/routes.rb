6c09e609ae619059a9c7e4d306b54018654ef793
i2 krispy <abdurtaalib@gmail.com>
2021-04-21 14:12:51 +0000

    e

--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-	
-	root "main_menu#index"
-
-	get "/main_menu", to: "main_menu#index"
-end

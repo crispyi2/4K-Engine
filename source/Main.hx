122d41e1fbcc2288d103afed68d112af3c666c59
crispyi2 <abdurtaalib@gmail.com>
2021-05-03 20:11:30 +0000

    test haxe's filesystem capabilities via nodejs

--- source/Main.hx
+++ source/Main.hx
@@ -2,12 +2,25 @@ package;
 
 import flixel.FlxGame;
 import openfl.display.Sprite;
+import sys.FileSystem;
+import sys.io.File;
 
 class Main extends Sprite
 {
+
+	static function main() {
+        var server = js.node.Net.createServer(function(socket) {
+            socket.write("Echo server\n\n");
+            socket.pipe(socket);
+        });
+        server.listen(8080, "0.0.0.0");
+    }
+
 	public function new()
 	{
 		super();
 		addChild(new FlxGame(0, 0, TitleScreenState));
+
+		FileSystem.createDirectory("./testDir");
 	}
 }
